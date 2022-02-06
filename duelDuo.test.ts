cc9559ab32a7cfad7ab286d5c9e8f31b73fb4935
Chris Puppe <chrispuppe@gmail.com>
2022-02-05 20:25:00 -0800

    Unit tesing done

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,16 +1,12 @@
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+const { Builder, Capabilities, By } = require('selenium-webdriver')
 
 require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
-
-afterAll(async () => {
-    driver.quit()
+    await driver.get('http://localhost:3000/')
 })
 
 test('Title shows up when page loads', async () => {
@@ -18,3 +14,8 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+afterAll(async () => {
+    await (await driver).quit()
+})
+
